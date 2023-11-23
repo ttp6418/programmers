72ec6599f2949f6a2d70558d5d69fca80f25f057
ttp6418 <123138007+ttp6418@users.noreply.github.com>
2023-11-23 12:31:25 +0900

    [level 2] Title: 짝지어 제거하기, Time: 178.72 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    tmp = []
+    for x in s:
+        tmp.append(x)
+        if len(tmp) > 1 and tmp[-1] == tmp[-2]:
+            tmp.pop()
+            tmp.pop()
+    if len(tmp) == 0: return 1
+    return 0
